6a924c24f73c189457998774cd76d93a460ad591
julian harley <julian.harley.fr@harpercollins.co.uk>
2019-08-18 11:29:31 +0100

    tag import now specifies start & end ids

--- policy/management/commands/populate_tags.py
+++ policy/management/commands/populate_tags.py
@@ -9,11 +9,25 @@ import textrazor
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('start_id', nargs='+', type=int)
+        parser.add_argument('end_id', nargs='+', type=int)
+
     def handle(self, *args, **options):
+        start_id, end_id = 0, Policy.objects.count()
+        if options['start_id']:
+            print(options['start_id'])
+            start_id = options['start_id'][0]
+        else:
+            start_id = 0
+        if options['end_id']:
+            end_id = options['end_id'][0]
+        else:
+            end_id = Policy.objects.count()
         logger.info("CALLED: populate tags")
         textrazor.api_key = settings.TEXTRAZOR_KEY
         client = textrazor.TextRazor(extractors=["topics"])
-        for policy in Policy.objects.all()[50:]:
+        for policy in Policy.objects.all()[start_id:end_id]:
             response = client.analyze(policy.text)
             for topic in response.topics():
                 print(topic.label, topic.score)
